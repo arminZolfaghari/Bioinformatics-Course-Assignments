4f6a907ffe96eabdb5f1c7f0cb6ec9518e7690bf
Armin Zolfaghari Dariani <49782087+arminZolfaghari@users.noreply.github.com>
2023-11-13 00:12:09 +0330

    Add HW 1

---
+++ HW 1 - Semi-Global Alignment/main.py
@@ -0,0 +1,241 @@
+PAM250 = {
+    'A': {'A': 2, 'C': -2, 'D': 0, 'E': 0, 'F': -3, 'G': 1, 'H': -1, 'I': -1, 'K': -1, 'L': -2, 'M': -1, 'N': 0, 'P': 1,
+          'Q': 0, 'R': -2, 'S': 1, 'T': 1, 'V': 0, 'W': -6, 'Y': -3},
+    'C': {'A': -2, 'C': 12, 'D': -5, 'E': -5, 'F': -4, 'G': -3, 'H': -3, 'I': -2, 'K': -5, 'L': -6, 'M': -5, 'N': -4,
+          'P': -3, 'Q': -5, 'R': -4, 'S': 0, 'T': -2, 'V': -2, 'W': -8, 'Y': 0},
+    'D': {'A': 0, 'C': -5, 'D': 4, 'E': 3, 'F': -6, 'G': 1, 'H': 1, 'I': -2, 'K': 0, 'L': -4, 'M': -3, 'N': 2, 'P': -1,
+          'Q': 2, 'R': -1, 'S': 0, 'T': 0, 'V': -2, 'W': -7, 'Y': -4},
+    'E': {'A': 0, 'C': -5, 'D': 3, 'E': 4, 'F': -5, 'G': 0, 'H': 1, 'I': -2, 'K': 0, 'L': -3, 'M': -2, 'N': 1, 'P': -1,
+          'Q': 2, 'R': -1, 'S': 0, 'T': 0, 'V': -2, 'W': -7, 'Y': -4},
+    'F': {'A': -3, 'C': -4, 'D': -6, 'E': -5, 'F': 9, 'G': -5, 'H': -2, 'I': 1, 'K': -5, 'L': 2, 'M': 0, 'N': -3,
+          'P': -5, 'Q': -5, 'R': -4, 'S': -3, 'T': -3, 'V': -1, 'W': 0, 'Y': 7},
+    'G': {'A': 1, 'C': -3, 'D': 1, 'E': 0, 'F': -5, 'G': 5, 'H': -2, 'I': -3, 'K': -2, 'L': -4, 'M': -3, 'N': 0, 'P': 0,
+          'Q': -1, 'R': -3, 'S': 1, 'T': 0, 'V': -1, 'W': -7, 'Y': -5},
+    'H': {'A': -1, 'C': -3, 'D': 1, 'E': 1, 'F': -2, 'G': -2, 'H': 6, 'I': -2, 'K': 0, 'L': -2, 'M': -2, 'N': 2, 'P': 0,
+          'Q': 3, 'R': 2, 'S': -1, 'T': -1, 'V': -2, 'W': -3, 'Y': 0},
+    'I': {'A': -1, 'C': -2, 'D': -2, 'E': -2, 'F': 1, 'G': -3, 'H': -2, 'I': 5, 'K': -2, 'L': 2, 'M': 2, 'N': -2,
+          'P': -2, 'Q': -2, 'R': -2, 'S': -1, 'T': 0, 'V': 4, 'W': -5, 'Y': -1},
+    'K': {'A': -1, 'C': -5, 'D': 0, 'E': 0, 'F': -5, 'G': -2, 'H': 0, 'I': -2, 'K': 5, 'L': -3, 'M': 0, 'N': 1, 'P': -1,
+          'Q': 1, 'R': 3, 'S': 0, 'T': 0, 'V': -2, 'W': -3, 'Y': -4},
+    'L': {'A': -2, 'C': -6, 'D': -4, 'E': -3, 'F': 2, 'G': -4, 'H': -2, 'I': 2, 'K': -3, 'L': 6, 'M': 4, 'N': -3,
+          'P': -3, 'Q': -2, 'R': -3, 'S': -3, 'T': -2, 'V': 2, 'W': -2, 'Y': -1},
+    'M': {'A': -1, 'C': -5, 'D': -3, 'E': -2, 'F': 0, 'G': -3, 'H': -2, 'I': 2, 'K': 0, 'L': 4, 'M': 6, 'N': -2,
+          'P': -2, 'Q': -1, 'R': 0, 'S': -2, 'T': -1, 'V': 2, 'W': -4, 'Y': -2},
+    'N': {'A': 0, 'C': -4, 'D': 2, 'E': 1, 'F': -3, 'G': 0, 'H': 2, 'I': -2, 'K': 1, 'L': -3, 'M': -2, 'N': 2, 'P': 0,
+          'Q': 1, 'R': 0, 'S': 1, 'T': 0, 'V': -2, 'W': -4, 'Y': -2},
+    'P': {'A': 1, 'C': -3, 'D': -1, 'E': -1, 'F': -5, 'G': 0, 'H': 0, 'I': -2, 'K': -1, 'L': -3, 'M': -2, 'N': 0,
+          'P': 6, 'Q': 0, 'R': 0, 'S': 1, 'T': 0, 'V': -1, 'W': -6, 'Y': -5},
+    'Q': {'A': 0, 'C': -5, 'D': 2, 'E': 2, 'F': -5, 'G': -1, 'H': 3, 'I': -2, 'K': 1, 'L': -2, 'M': -1, 'N': 1, 'P': 0,
+          'Q': 4, 'R': 1, 'S': -1, 'T': -1, 'V': -2, 'W': -5, 'Y': -4},
+    'R': {'A': -2, 'C': -4, 'D': -1, 'E': -1, 'F': -4, 'G': -3, 'H': 2, 'I': -2, 'K': 3, 'L': -3, 'M': 0, 'N': 0,
+          'P': 0, 'Q': 1, 'R': 6, 'S': 0, 'T': -1, 'V': -2, 'W': 2, 'Y': -4},
+    'S': {'A': 1, 'C': 0, 'D': 0, 'E': 0, 'F': -3, 'G': 1, 'H': -1, 'I': -1, 'K': 0, 'L': -3, 'M': -2, 'N': 1, 'P': 1,
+          'Q': -1, 'R': 0, 'S': 2, 'T': 1, 'V': -1, 'W': -2, 'Y': -3},
+    'T': {'A': 1, 'C': -2, 'D': 0, 'E': 0, 'F': -3, 'G': 0, 'H': -1, 'I': 0, 'K': 0, 'L': -2, 'M': -1, 'N': 0, 'P': 0,
+          'Q': -1, 'R': -1, 'S': 1, 'T': 3, 'V': 0, 'W': -5, 'Y': -3},
+    'V': {'A': 0, 'C': -2, 'D': -2, 'E': -2, 'F': -1, 'G': -1, 'H': -2, 'I': 4, 'K': -2, 'L': 2, 'M': 2, 'N': -2,
+          'P': -1, 'Q': -2, 'R': -2, 'S': -1, 'T': 0, 'V': 4, 'W': -6, 'Y': -2},
+    'W': {'A': -6, 'C': -8, 'D': -7, 'E': -7, 'F': 0, 'G': -7, 'H': -3, 'I': -5, 'K': -3, 'L': -2, 'M': -4, 'N': -4,
+          'P': -6, 'Q': -5, 'R': 2, 'S': -2, 'T': -5, 'V': -6, 'W': 17, 'Y': 0},
+    'Y': {'A': -3, 'C': 0, 'D': -4, 'E': -4, 'F': 7, 'G': -5, 'H': 0, 'I': -1, 'K': -4, 'L': -1, 'M': -2, 'N': -2,
+          'P': -5, 'Q': -4, 'R': -4, 'S': -3, 'T': -3, 'V': -2, 'W': 0, 'Y': 10}
+}
+GAP_PENALTY = 9
+
+
+def my_print(score, seq):
+    print(score)
+    sorted_seq = [i[0] + i[1] for i in seq]
+    sorted_seq.sort()
+    for i in sorted_seq:
+        print(i[0:int(len(i) / 2)])
+        print(i[int(len(i) / 2):])
+
+
+def printf(matrix):
+    for i in range(len(matrix)):
+        print(matrix[i])
+
+
+def get_column(matrix, i):
+    return [row[i] for row in matrix]
+
+
+def initial_matrix(protein1, protein2):
+    number_of_rows = len(protein1)
+    number_of_columns = len(protein2)
+    matrix = []
+    for i in range(number_of_rows + 1):
+        matrix.append([0] * (number_of_columns + 1))
+
+    return matrix
+    # return [[0] * (number_of_columns + 1)] * (number_of_rows + 1)
+
+
+def recursion(point, direction_matrix, path, paths):
+    (i, j) = point
+    if direction_matrix[i][j] != 0:
+        if direction_matrix[i][j] == 'd':
+            return recursion((i - 1, j - 1), direction_matrix, 'd' + path, paths)
+        if direction_matrix[i][j] == 'u':
+            return recursion((i - 1, j), direction_matrix, 'u' + path, paths)
+        if direction_matrix[i][j] == 'l':
+            return recursion((i, j - 1), direction_matrix, 'l' + path, paths)
+        if direction_matrix[i][j] == 'du':
+            p1 = recursion((i - 1, j - 1), direction_matrix, 'd' + path, paths)
+            p2 = recursion((i - 1, j), direction_matrix, 'u' + path, paths)
+            return p2
+            # return [recursion((i - 1, j - 1), direction_matrix, 'd' + path),
+            #         recursion((i - 1, j), direction_matrix, 'u' + path)]
+
+        if direction_matrix[i][j] == 'dl':
+            p1 = recursion((i - 1, j - 1), direction_matrix, 'd' + path, paths)
+            p2 = recursion((i, j - 1), direction_matrix, 'l' + path, paths)
+            return p2
+            # return [recursion((i - 1, j - 1), direction_matrix, 'd' + path),
+            #         recursion((i, j - 1), direction_matrix, 'l' + path)]
+
+        if direction_matrix[i][j] == 'ul':
+            p1 = recursion((i - 1, j), direction_matrix, 'u' + path, paths)
+            p2 = recursion((i, j - 1), direction_matrix, 'l' + path, paths)
+            return p2
+            # return [recursion((i - 1, j), direction_matrix, 'u' + path),
+            #         recursion((i, j - 1), direction_matrix, 'l' + path)]
+
+        if direction_matrix[i][j] == 'dul':
+            p1 = recursion((i - 1, j - 1), direction_matrix, 'd' + path, paths)
+            p2 = recursion((i - 1, j), direction_matrix, 'u' + path, paths)
+            p3 = recursion((i, j - 1), direction_matrix, 'l' + path, paths)
+            return p3
+            # return [recursion((i - 1, j - 1), direction_matrix, 'd' + path),
+            #         recursion((i - 1, j), direction_matrix, 'u' + path),
+            #         recursion((i, j - 1), direction_matrix, 'l' + path)]
+
+    else:
+        paths.append(path)
+        return paths
+
+
+def backtrack(final_points, direction_matrix):
+    paths = {}
+    for point in final_points:
+        p = recursion(point, direction_matrix, '', [])
+        paths[point] = p
+    return paths
+
+
+def create_seq(protein1, protein2, paths, direction_matrix):
+    number_of_rows = len(direction_matrix)
+    number_of_columns = len(direction_matrix[0])
+    protein1_arr = list(protein1)
+    protein2_arr = list(protein2)
+    seqs = []
+    for point, paths in paths.items():
+        for path in paths:
+            (i, j) = point
+            reverse_path = path[::-1]
+            seq_p1, seq_p2 = '', ''
+
+            if i == number_of_rows - 1 and j != number_of_columns - 1:
+                for k in range(number_of_columns - 1 - j):
+                    seq_p2 = protein2_arr[number_of_columns - k - 2] + seq_p2
+                    seq_p1 = '-' + seq_p1
+
+            if j == number_of_columns - 1 and i != number_of_rows - 1:
+                for k in range(number_of_rows - 1 - i):
+                    seq_p1 = protein1_arr[number_of_rows - k - 2] + seq_p1
+                    seq_p2 = '-' + seq_p2
+
+            for char in reverse_path:
+                if char == 'd':
+                    seq_p1 = protein1_arr[i - 1] + seq_p1
+                    seq_p2 = protein2_arr[j - 1] + seq_p2
+                    i = i - 1
+                    j = j - 1
+                if char == 'u':
+                    seq_p1 = protein1_arr[i - 1] + seq_p1
+                    seq_p2 = '-' + seq_p2
+                    i = i - 1
+                if char == 'l':
+                    seq_p1 = '-' + seq_p1
+                    seq_p2 = protein2_arr[j - 1] + seq_p2
+                    j = j - 1
+                if direction_matrix[i][j] == 0:
+                    if i == 0 and j != 0:
+                        for k in range(j):
+                            seq_p1 = '-' + seq_p1
+                            seq_p2 = protein2_arr[j - 1] + seq_p2
+                            j -= 1
+
+                    if j == 0 and i != 0:
+                        for k in range(i):
+                            seq_p1 = protein1_arr[i - 1] + seq_p1
+                            seq_p2 = '-' + seq_p2
+                            i -= 1
+
+            seqs.append((seq_p1, seq_p2))
+    return seqs
+
+
+def find_max_score(score_matrix):
+    final_points = []
+    last_row = score_matrix[-1]
+    last_column = get_column(score_matrix, len(score_matrix[0]) - 1)
+    max_in_last_row = max(last_row)
+    max_in_last_column = max(last_column)
+    max_score = max([max_in_last_row, max_in_last_column])
+
+    for j in range(len(last_row) - 1):
+        if last_row[j] == max_score:
+            point = (len(score_matrix) - 1, j)
+            final_points.append(point)
+
+    for i in range(len(last_column)):
+        if last_column[i] == max_score:
+            point = (i, len(score_matrix[0]) - 1)
+            final_points.append(point)
+
+    return max_score, final_points
+
+
+def semi_global_alignment_algorithm(protein1, protein2, score_matrix, direction_matrix):
+    protein1_arr = list(protein1)
+    protein2_arr = list(protein2)
+
+    for i in range(1, len(score_matrix)):
+        for j in range(1, len(score_matrix[0])):
+            char_p1 = protein1_arr[i - 1]
+            char_p2 = protein2_arr[j - 1]
+            diameter_score = score_matrix[i - 1][j - 1] + PAM250[char_p1][char_p2]
+            up_score = score_matrix[i - 1][j] - GAP_PENALTY
+            left_score = score_matrix[i][j - 1] - GAP_PENALTY
+
+            max_score = max(diameter_score, up_score, left_score)
+            direction = ''
+            if max_score == diameter_score:
+                direction += 'd'
+            if max_score == up_score:
+                direction += 'u'
+            if max_score == left_score:
+                direction += 'l'
+
+            # update score_matrix and direction_matrix
+            score_matrix[i][j] = max_score
+            direction_matrix[i][j] = direction
+
+    max_score, final_points = find_max_score(score_matrix)
+
+    paths = backtrack(final_points, direction_matrix)
+    # print(paths)
+
+    seqs = create_seq(protein1, protein2, paths, direction_matrix)
+    my_print(max_score, seqs)
+
+
+if __name__ == '__main__':
+    protein1 = input()
+    protein2 = input()
+
+    score_matrix = initial_matrix(protein1, protein2)
+    direction_matrix = initial_matrix(protein1, protein2)
+    semi_global_alignment_algorithm(protein1, protein2, score_matrix, direction_matrix)
